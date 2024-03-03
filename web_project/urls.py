fc923bde394bf9d3d049b7ca35f4f27a98988dc9
AlmondSlinky519 <isaacferry5@gmail.com>
2024-03-02 16:51:42 -0800

    Working with Kyle's page

--- web_project/urls.py
+++ web_project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render
+from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
 ]
