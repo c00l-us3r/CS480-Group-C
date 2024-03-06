ce01d4a731f41d01bd0f50bffdce05e7bb2f758a
Sammay206 <starforce206@gmail.com>
2024-03-06 13:32:30 -0800

    created API Interface and Text Processor

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\isaac\OneDrive\Documents\GitHub\CS480-Group-C\.venv
+set VIRTUAL_ENV=C:\Users\starf\Documents\GitHub\CS480-Group-C\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
