2250e017e6f366ea46f1642d8805337a0934b4e8
Isaac Ferry <isaacferry5@gmail.com>
2024-03-07 13:14:18 -0800

    Added Anthony's html/css files
    Added anthony's Html/css file by copy and pasting the code into my folders.

--- stocks/urls.py
+++ stocks/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     # Import the home view for the stocks app
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('detailed', views.detailed, name="detailed"),
 ]
--- stocks/views.py
+++ stocks/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'stocks/base.html', {}) # Render the starting base.html page from the stocks app
+    return render(request, 'stocks/base.html', {}), # Render the starting base.html page from the stocks app
+
+
+def detailed(request):
+    return render(request, 'stocks/detailed.html', {}), # Render the detailed.html page from the stocks app
