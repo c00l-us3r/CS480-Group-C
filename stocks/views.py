404d3bae895eb0d7bf75372b5c41d519dc51026d
AlmondSlinky519 <isaacferry5@gmail.com>
2024-03-13 14:57:51 -0700

    working back to home page

--- stocks/views.py
+++ stocks/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'stocks/base.html', {}), # Render the starting base.html page from the stocks app
+    return render(request, 'stocks/base.html'), # Render the starting base.html page from the stocks app
 
 
 
